17e26dc3e8cd5d47f6667b88fe576f4c915beb70
mohammaDJ23 <mohammad.ha9bi8@gmail.com>
2023-09-21 11:06:23 +0330

    using HtmlWebpackPlugin for prod and stg

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,11 +1,13 @@
 const { merge } = require('webpack-merge');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const commonConfig = require('./webpack.common');
 
 module.exports = merge(commonConfig, {
   mode: 'production',
   plugins: [
+    new HtmlWebpackPlugin({ template: './public/index.html' }),
     new webpack.DefinePlugin({
       'process.env': JSON.stringify(process.env),
     }),
