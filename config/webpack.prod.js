94df11721d86b904cade313e271fdd7e725c5120
mohammaDJ23 <mohammad.ha9bi8@gmail.com>
2023-09-21 10:53:48 +0330

    adding the favicons-webpack-plugin pacakge

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,5 +1,6 @@
 const { merge } = require('webpack-merge');
 const webpack = require('webpack');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const commonConfig = require('./webpack.common');
 
 module.exports = merge(commonConfig, {
@@ -7,6 +8,17 @@ module.exports = merge(commonConfig, {
   plugins: [
     new webpack.DefinePlugin({
       'process.env': JSON.stringify(process.env),
+    }),
+    new FaviconsWebpackPlugin({
+      logo: './public/app-logo-48.png',
+      favicons: {
+        path: `${process.env.BANK_APP}/bank/static/assets/`,
+        icons: {
+          android: true,
+          appleIcon: true,
+          favicons: true,
+        },
+      },
     }),
   ],
   output: {
