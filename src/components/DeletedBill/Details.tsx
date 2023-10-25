21a41b3447ca7b8fbb5c90ba5f7bc64f80621ce7
mohammaDJ23 <mohammad.ha9bi8@gmail.com>
2023-10-25 23:27:52 +0330

    revising the ui of deleted bill

--- src/components/DeletedBill/Details.tsx
+++ src/components/DeletedBill/Details.tsx
@@ -29,7 +29,7 @@ const Details: FC<DetailsImporation> = ({ bill }) => {
   }, []);
 
   const restoreBill = useCallback(() => {
-    request(new RestoreBillApi(bill.id)).then(response => {
+    request(new RestoreBillApi(bill.id)).then((response) => {
       navigate(Pathes.BILLS);
     });
   }, [bill]);
@@ -38,24 +38,39 @@ const Details: FC<DetailsImporation> = ({ bill }) => {
     <>
       <Box width="100%" display="flex" flexDirection="column" alignItems="start" gap="8px">
         <Box width="100%" mb="15px" display="flex" gap="8px" justifyContent="space-between" alignItems="center">
-          <Typography fontWeight="700" fontSize="14px">
+          <Typography component={'p'} fontSize="14px" fontWeight={'bold'}>
             {bill.amount}
           </Typography>
         </Box>
-        <Typography fontSize="12px" color="">
-          receiver: {bill.receiver}
+        <Typography component={'p'} fontSize="12px" color="rgba(0, 0, 0, 0.6)">
+          <Typography component={'span'} fontSize="12px" fontWeight={'bold'} color={'black'}>
+            Receiver:
+          </Typography>{' '}
+          {bill.receiver}
         </Typography>
-        <Typography fontSize="12px" color="">
-          description: {bill.description}
+        <Typography component={'p'} fontSize="12px" color="rgba(0, 0, 0, 0.6)">
+          <Typography component={'span'} fontSize="12px" fontWeight={'bold'} color={'black'}>
+            Description:
+          </Typography>{' '}
+          {bill.description}
         </Typography>
-        <Typography fontSize="12px" color="">
-          received at: {moment(bill.date).format('LL')}
+        <Typography component={'p'} fontSize="12px" color="rgba(0, 0, 0, 0.6)">
+          <Typography component={'span'} fontSize="12px" fontWeight={'bold'} color={'black'}>
+            Received at:
+          </Typography>{' '}
+          {moment(bill.date).format('LL')}
         </Typography>
-        <Typography fontSize="12px" color="">
-          created at: {moment(bill.createdAt).format('LLLL')}
+        <Typography component={'p'} fontSize="12px" color="rgba(0, 0, 0, 0.6)">
+          <Typography component={'span'} fontSize="12px" fontWeight={'bold'} color={'black'}>
+            Created at:
+          </Typography>{' '}
+          {moment(bill.createdAt).format('LLLL')}
         </Typography>
-        <Typography fontSize="12px" color="">
-          was deleted at: {moment(bill.deletedAt).format('LLLL')}
+        <Typography component={'p'} fontSize="12px" color="rgba(0, 0, 0, 0.6)">
+          <Typography component={'span'} fontSize="12px" fontWeight={'bold'} color={'black'}>
+            Created at:
+          </Typography>{' '}
+          {moment(bill.deletedAt).format('LLLL')}
         </Typography>
         <Box mt="30px">
           <Button
