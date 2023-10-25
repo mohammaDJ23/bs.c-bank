d9320653e05725b86ca095ac57a852fedf3f0a2f
mohammaDJ23 <mohammad.ha9bi8@gmail.com>
2023-10-25 22:59:12 +0330

    revising the ui of the user

--- src/components/User/Details.tsx
+++ src/components/User/Details.tsx
@@ -97,7 +97,15 @@ const Details: FC<DetailsImporation> = ({ user }) => {
     <>
       <Box width="100%" display="flex" flexDirection="column" alignItems="start" gap="8px">
         <Box width="100%" mb="15px" display="flex" gap="8px" justifyContent="space-between" alignItems="center">
-          <Box component={'div'} display="flex" alignItems="center" justifyContent="start" gap="10px" flexWrap="wrap">
+          <Box
+            component={'div'}
+            display="flex"
+            alignItems="center"
+            justifyContent="start"
+            gap="10px"
+            flexWrap="wrap"
+            mb={'15px'}
+          >
             {isUserOwner && (
               <Box
                 sx={{
@@ -110,7 +118,7 @@ const Details: FC<DetailsImporation> = ({ user }) => {
                 component={'span'}
               ></Box>
             )}
-            <Typography fontWeight="700" fontSize="16px">
+            <Typography component={'p'} fontSize="14px" fontWeight={'bold'}>
               {user.firstName} {user.lastName}
             </Typography>
           </Box>
@@ -129,60 +137,93 @@ const Details: FC<DetailsImporation> = ({ user }) => {
             </>
           )}
         </Box>
-        <Typography fontSize="12px" color="">
-          email: {user.email}
+        <Typography component={'p'} fontSize="12px" color="rgba(0, 0, 0, 0.6)">
+          <Typography component={'span'} fontSize="12px" fontWeight={'bold'} color={'black'}>
+            Email:
+          </Typography>{' '}
+          {user.email}
         </Typography>
-        <Typography fontSize="12px" color="">
-          phone: {user.phone}
+        <Typography component={'p'} fontSize="12px" color="rgba(0, 0, 0, 0.6)">
+          <Typography component={'span'} fontSize="12px" fontWeight={'bold'} color={'black'}>
+            Phone:
+          </Typography>{' '}
+          {user.phone}
         </Typography>
-        <Typography fontSize="12px" color="">
-          role: {user.role}
+        <Typography component={'p'} fontSize="12px" color="rgba(0, 0, 0, 0.6)">
+          <Typography component={'span'} fontSize="12px" fontWeight={'bold'} color={'black'}>
+            Role:
+          </Typography>{' '}
+          {user.role}
         </Typography>
-        <Typography fontSize="12px" color="">
-          created by: {user.parent.firstName} {user.parent.lastName} ({user.parent.role}){' '}
+        <Typography component={'p'} fontSize="12px" color="rgba(0, 0, 0, 0.6)">
+          <Typography component={'span'} fontSize="12px" fontWeight={'bold'} color={'black'}>
+            Created by:
+          </Typography>{' '}
+          {user.parent.firstName} {user.parent.lastName} ({user.parent.role}){' '}
           {user.parent.deletedAt && `was deleted at ${moment(user.parent.deletedAt).format('LLLL')}`}
         </Typography>
         {isCurrentUserOwner && (
-          <Typography fontSize="12px" color="">
-            total created users: {user.users.quantities}
+          <Typography component={'p'} fontSize="12px" color="rgba(0, 0, 0, 0.6)">
+            <Typography component={'span'} fontSize="12px" fontWeight={'bold'} color={'black'}>
+              Total created users:
+            </Typography>{' '}
+            {user.users.quantities}
           </Typography>
         )}
-        <Typography fontSize="12px" color="">
-          total bill quantities: {user.bill.counts}
+        <Typography component={'p'} fontSize="12px" color="rgba(0, 0, 0, 0.6)">
+          <Typography component={'span'} fontSize="12px" fontWeight={'bold'} color={'black'}>
+            Total bill quantities:
+          </Typography>{' '}
+          {user.bill.counts}
         </Typography>
-        <Typography fontSize="12px" color="">
-          total bill amounts: {user.bill.amounts}
+        <Typography component={'p'} fontSize="12px" color="rgba(0, 0, 0, 0.6)">
+          <Typography component={'span'} fontSize="12px" fontWeight={'bold'} color={'black'}>
+            Total bill amounts:
+          </Typography>{' '}
+          {user.bill.amounts}
         </Typography>
         {isUserOwner &&
           (() => {
             const userLastConnection = getUserLastConnection(user.id);
             if (userLastConnection) {
               return (
-                <Typography fontSize="12px" color="">
-                  last connection: {moment(userLastConnection).format('LLLL')}
+                <Typography component={'p'} fontSize="12px" color="rgba(0, 0, 0, 0.6)">
+                  <Typography component={'span'} fontSize="12px" fontWeight={'bold'} color={'black'}>
+                    Last connection:
+                  </Typography>{' '}
+                  {moment(userLastConnection).format('LLLL')}
                 </Typography>
               );
             } else if (userLastConnection === undefined) {
               return (
-                <Typography fontSize="12px" color="">
-                  last connection: long time ago
+                <Typography component={'p'} fontSize="12px" color="rgba(0, 0, 0, 0.6)">
+                  <Typography component={'span'} fontSize="12px" fontWeight={'bold'} color={'black'}>
+                    Last connection:
+                  </Typography>{' '}
+                  long time ago
                 </Typography>
               );
             }
             return <></>;
           })()}
-        <Typography fontSize="12px" color="">
-          created at: {moment(user.createdAt).format('LLLL')}
+        <Typography component={'p'} fontSize="12px" color="rgba(0, 0, 0, 0.6)">
+          <Typography component={'span'} fontSize="12px" fontWeight={'bold'} color={'black'}>
+            Created at:
+          </Typography>{' '}
+          {moment(user.createdAt).format('LLLL')}
         </Typography>
         {new Date(user.updatedAt) > new Date(user.createdAt) && (
-          <Typography fontSize="12px" color="">
-            last update: {moment(user.updatedAt).format('LLLL')}
+          <Typography component={'p'} fontSize="12px" color="rgba(0, 0, 0, 0.6)">
+            <Typography component={'span'} fontSize="12px" fontWeight={'bold'} color={'black'}>
+              Last update:
+            </Typography>{' '}
+            {moment(user.updatedAt).format('LLLL')}
           </Typography>
         )}
         {isAuthorized && (
           <Box display="flex" alignItems="center" gap="8px">
-            <Typography fontSize="12px" color="">
-              the bill report:
+            <Typography component={'span'} fontSize="12px" fontWeight={'bold'} color={'black'}>
+              The bill report:
             </Typography>
             <Box display="flex" alignItems="center" gap="10px">
               <Typography
