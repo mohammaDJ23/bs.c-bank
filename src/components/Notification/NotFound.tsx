8ce982112af41917a4b4c4ca4a5baf4a8dcfaa2e
mohammaDJ23 <mohammad.ha9bi8@gmail.com>
2024-01-21 18:57:47 +0330

    revising the mt

--- src/components/Notification/NotFound.tsx
+++ src/components/Notification/NotFound.tsx
@@ -7,7 +7,7 @@ const NotFound: FC = () => {
   const navigate = useNavigate();
 
   return (
-    <Box display="flex" alignItems="center" justifyContent="center" flexDirection="column" gap="10px" mt="20px">
+    <Box display="flex" alignItems="center" justifyContent="center" flexDirection="column" gap="10px" mt="50px">
       <Typography fontSize={'16px'} fontWeight={'500'}>
         Not found the notification
       </Typography>
