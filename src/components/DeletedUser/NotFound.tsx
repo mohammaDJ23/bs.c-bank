ff059787efa3cb55c3659688de158fdbc3f48ff9
mohammaDJ23 <mohammad.ha9bi8@gmail.com>
2024-02-02 07:01:20 +0330

    removing the extra margin-top

--- src/components/DeletedUser/NotFound.tsx
+++ src/components/DeletedUser/NotFound.tsx
@@ -11,7 +11,7 @@ const NotFound: FC = () => {
   const decodedTOken = auth.getDecodedToken()!;
 
   return (
-    <Box display="flex" alignItems="center" justifyContent="center" flexDirection="column" gap="10px" mt="50px">
+    <Box display="flex" alignItems="center" justifyContent="center" flexDirection="column" gap="10px" mt="34px">
       <Typography fontSize={'16px'} fontWeight={'500'}>
         Not found the user
       </Typography>
--- src/components/UpdateUser/NotFound.tsx
+++ src/components/UpdateUser/NotFound.tsx
@@ -10,7 +10,7 @@ const NotFound: FC = () => {
   const decodedToken = auth.getDecodedToken()!;
 
   return (
-    <Box display="flex" alignItems="center" justifyContent="center" flexDirection="column" gap="10px" mt="50px">
+    <Box display="flex" alignItems="center" justifyContent="center" flexDirection="column" gap="10px" mt="34px">
       <Typography fontSize={'16px'} fontWeight={'500'}>
         Not found the user
       </Typography>
