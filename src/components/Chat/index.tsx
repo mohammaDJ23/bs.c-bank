31a5ef8e6809510e29833b5f42aa652ef94fbf47
mohammaDJ23 <mohammad.ha9bi8@gmail.com>
2024-02-08 08:49:08 +0330

    adding more logic for resizing the chat ui

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -2,6 +2,19 @@ import { FC } from 'react';
 import { app } from 'chat/ChatApp';
 import { useAuth, useInitialMicro } from '../../hooks';
 import Navigation from '../../layout/Navigation';
+import { Box, styled } from '@mui/material';
+
+const ChatContentWrapper = styled(Box)(({ theme }) => ({
+  width: '100%',
+  height: '100%',
+  marginTop: '-64px',
+  paddingTop: '64px',
+
+  [theme.breakpoints.down('sm')]: {
+    marginTop: '-48px',
+    paddingTop: '48px',
+  },
+}));
 
 const ChatContent: FC = () => {
   const initialMicro = useInitialMicro(app);
@@ -9,9 +22,10 @@ const ChatContent: FC = () => {
   const isCurrentOwner = auth.isCurrentOwner();
 
   return (
-    <Navigation title={isCurrentOwner ? 'Conversations' : 'Contact support'}>
-      <div ref={initialMicro.ref} />
-    </Navigation>
+    <Box sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+      <Navigation title={isCurrentOwner ? 'Conversations' : 'Contact support'}></Navigation>
+      <ChatContentWrapper ref={initialMicro.ref} />
+    </Box>
   );
 };
 
