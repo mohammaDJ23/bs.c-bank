6b5619fca9580d533b9c7e56c1007073d3b7c1d6
mohammaDJ23 <mohammad.ha9bi8@gmail.com>
2024-08-31 11:33:44 +0330

    revising the animation of deletedUser

--- src/components/DeletedUser/Details.tsx
+++ src/components/DeletedUser/Details.tsx
@@ -8,6 +8,7 @@ import { User, Pathes } from '../../lib';
 import { ModalNames } from '../../store';
 import { useNavigate } from 'react-router-dom';
 import { useSnackbar } from 'notistack';
+import ResetStyleWithAnimation from '../shared/ResetStyleWithAnimation';
 
 interface DetailsImporation {
   user: User;
@@ -50,60 +51,163 @@ const Details: FC<DetailsImporation> = ({ user }) => {
   return (
     <>
       <Box width="100%" display="flex" flexDirection="column" alignItems="start" gap="8px">
-        <Box width="100%" mb="15px" display="flex" gap="8px" justifyContent="space-between" alignItems="center">
-          <Typography component={'p'} fontSize="14px" fontWeight={'bold'}>
-            {user.firstName} {user.lastName}
-          </Typography>
-        </Box>
-        <Typography component={'p'} fontSize="12px" color="rgba(0, 0, 0, 0.6)">
-          <Typography component={'span'} fontSize="12px" fontWeight={'bold'} color={'black'}>
-            Email:
-          </Typography>{' '}
-          {user.email}
-        </Typography>
-        <Typography component={'p'} fontSize="12px" color="rgba(0, 0, 0, 0.6)">
-          <Typography component={'span'} fontSize="12px" fontWeight={'bold'} color={'black'}>
-            Phone:
-          </Typography>{' '}
-          {user.phone}
-        </Typography>
-        <Typography component={'p'} fontSize="12px" color="rgba(0, 0, 0, 0.6)">
-          <Typography component={'span'} fontSize="12px" fontWeight={'bold'} color={'black'}>
-            Role:
-          </Typography>{' '}
-          {user.role}
-        </Typography>
-        <Typography component={'p'} fontSize="12px" color="rgba(0, 0, 0, 0.6)">
-          <Typography component={'span'} fontSize="12px" fontWeight={'bold'} color={'black'}>
-            Created by:
-          </Typography>{' '}
-          {user.parent.firstName} {user.parent.lastName} ({user.parent.role}){' '}
-          {user.parent.deletedAt && `was deleted at ${moment(user.parent.deletedAt).format('LLLL')}`}
-        </Typography>
-        <Typography component={'p'} fontSize="12px" color="rgba(0, 0, 0, 0.6)">
-          <Typography component={'span'} fontSize="12px" fontWeight={'bold'} color={'black'}>
-            Created at:
-          </Typography>{' '}
-          {moment(user.createdAt).format('LLLL')}
-        </Typography>
-        <Typography component={'p'} fontSize="12px" color="rgba(0, 0, 0, 0.6)">
-          <Typography component={'span'} fontSize="12px" fontWeight={'bold'} color={'black'}>
-            Deleted at:
-          </Typography>{' '}
-          {moment(user.deletedAt).format('LLLL')}
-        </Typography>
-        {isUserCreatedByCurrentUser && (
-          <Box mt="30px">
-            <Button
-              disabled={isRestoreUserApiProcessing}
-              onClick={showRestoreUserModal}
-              variant="contained"
-              color="success"
-              size="small"
-              sx={{ textTransform: 'capitalize' }}
+        <Box overflow="hidden" mb="15px">
+          <ResetStyleWithAnimation sx={{ transform: 'translateY(0)' }}>
+            <Box
+              sx={{
+                transform: 'translateY(100%)',
+                transition: 'cubic-bezier(.41,.55,.03,.96) 1s',
+              }}
+              width="100%"
+              display="flex"
+              gap="8px"
+              justifyContent="space-between"
+              alignItems="center"
             >
-              Restore the user
-            </Button>
+              <Typography component={'p'} fontSize="14px" fontWeight={'bold'}>
+                {user.firstName} {user.lastName}
+              </Typography>
+            </Box>
+          </ResetStyleWithAnimation>
+        </Box>
+        <Box overflow="hidden">
+          <ResetStyleWithAnimation sx={{ transform: 'translateY(0)' }}>
+            <Typography
+              sx={{
+                transform: 'translateY(100%)',
+                transition: 'cubic-bezier(.41,.55,.03,.96) 1s',
+                transitionDelay: '0.02s',
+              }}
+              component={'p'}
+              fontSize="12px"
+              color="rgba(0, 0, 0, 0.6)"
+            >
+              <Typography component={'span'} fontSize="12px" fontWeight={'bold'} color={'black'}>
+                Email:
+              </Typography>{' '}
+              {user.email}
+            </Typography>
+          </ResetStyleWithAnimation>
+        </Box>
+        <Box overflow="hidden">
+          <ResetStyleWithAnimation sx={{ transform: 'translateY(0)' }}>
+            <Typography
+              sx={{
+                transform: 'translateY(100%)',
+                transition: 'cubic-bezier(.41,.55,.03,.96) 1s',
+                transitionDelay: '0.04s',
+              }}
+              component={'p'}
+              fontSize="12px"
+              color="rgba(0, 0, 0, 0.6)"
+            >
+              <Typography component={'span'} fontSize="12px" fontWeight={'bold'} color={'black'}>
+                Phone:
+              </Typography>{' '}
+              {user.phone}
+            </Typography>
+          </ResetStyleWithAnimation>
+        </Box>
+        <Box overflow="hidden">
+          <ResetStyleWithAnimation sx={{ transform: 'translateY(0)' }}>
+            <Typography
+              sx={{
+                transform: 'translateY(100%)',
+                transition: 'cubic-bezier(.41,.55,.03,.96) 1s',
+                transitionDelay: '0.06s',
+              }}
+              component={'p'}
+              fontSize="12px"
+              color="rgba(0, 0, 0, 0.6)"
+            >
+              <Typography component={'span'} fontSize="12px" fontWeight={'bold'} color={'black'}>
+                Role:
+              </Typography>{' '}
+              {user.role}
+            </Typography>
+          </ResetStyleWithAnimation>
+        </Box>
+        <Box overflow="hidden">
+          <ResetStyleWithAnimation sx={{ transform: 'translateY(0)' }}>
+            <Typography
+              sx={{
+                transform: 'translateY(100%)',
+                transition: 'cubic-bezier(.41,.55,.03,.96) 1s',
+                transitionDelay: '0.08s',
+              }}
+              component={'p'}
+              fontSize="12px"
+              color="rgba(0, 0, 0, 0.6)"
+            >
+              <Typography component={'span'} fontSize="12px" fontWeight={'bold'} color={'black'}>
+                Created by:
+              </Typography>{' '}
+              {user.parent.firstName} {user.parent.lastName} ({user.parent.role}){' '}
+              {user.parent.deletedAt && `was deleted at ${moment(user.parent.deletedAt).format('LLLL')}`}
+            </Typography>
+          </ResetStyleWithAnimation>
+        </Box>
+        <Box overflow="hidden">
+          <ResetStyleWithAnimation sx={{ transform: 'translateY(0)' }}>
+            <Typography
+              sx={{
+                transform: 'translateY(100%)',
+                transition: 'cubic-bezier(.41,.55,.03,.96) 1s',
+                transitionDelay: '0.1s',
+              }}
+              component={'p'}
+              fontSize="12px"
+              color="rgba(0, 0, 0, 0.6)"
+            >
+              <Typography component={'span'} fontSize="12px" fontWeight={'bold'} color={'black'}>
+                Created at:
+              </Typography>{' '}
+              {moment(user.createdAt).format('LLLL')}
+            </Typography>
+          </ResetStyleWithAnimation>
+        </Box>
+        <Box overflow="hidden">
+          <ResetStyleWithAnimation sx={{ transform: 'translateY(0)' }}>
+            <Typography
+              sx={{
+                transform: 'translateY(100%)',
+                transition: 'cubic-bezier(.41,.55,.03,.96) 1s',
+                transitionDelay: '0.12s',
+              }}
+              component={'p'}
+              fontSize="12px"
+              color="rgba(0, 0, 0, 0.6)"
+            >
+              <Typography component={'span'} fontSize="12px" fontWeight={'bold'} color={'black'}>
+                Deleted at:
+              </Typography>{' '}
+              {moment(user.deletedAt).format('LLLL')}
+            </Typography>
+          </ResetStyleWithAnimation>
+        </Box>
+        {isUserCreatedByCurrentUser && (
+          <Box overflow="hidden">
+            <ResetStyleWithAnimation sx={{ transform: 'translateY(0)' }}>
+              <Box
+                sx={{
+                  transform: 'translateY(100%)',
+                  transition: 'cubic-bezier(.41,.55,.03,.96) 1s',
+                  transitionDelay: '0.14s',
+                }}
+                mt="30px"
+              >
+                <Button
+                  disabled={isRestoreUserApiProcessing}
+                  onClick={showRestoreUserModal}
+                  variant="contained"
+                  color="success"
+                  size="small"
+                  sx={{ textTransform: 'capitalize' }}
+                >
+                  Restore the user
+                </Button>
+              </Box>
+            </ResetStyleWithAnimation>
           </Box>
         )}
       </Box>
