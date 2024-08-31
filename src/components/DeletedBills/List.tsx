062f44c896cfd91d4dbf80889012bc06e3c02ca3
mohammaDJ23 <mohammad.ha9bi8@gmail.com>
2024-08-31 11:21:30 +0330

    revising the animation of deletedBIlls

--- src/components/DeletedBills/List.tsx
+++ src/components/DeletedBills/List.tsx
@@ -11,6 +11,7 @@ import { ModalNames } from '../../store';
 import BillCard from '../shared/BiilCard';
 import { selectDeletedBillsList } from '../../store/selectors';
 import { useSnackbar } from 'notistack';
+import ResetStyleWithAnimation from '../shared/ResetStyleWithAnimation';
 
 const List: FC = () => {
   const request = useRequest();
@@ -81,7 +82,18 @@ const List: FC = () => {
         <>
           <MuiList>
             {deletedBillsList.list.map((bill, index) => (
-              <BillCard key={index} index={index} bill={bill} list={deletedBillsList} />
+              <ResetStyleWithAnimation key={index} sx={{ opacity: '1', transform: 'translateY(0)' }}>
+                <Box
+                  sx={{
+                    opacity: '0',
+                    transform: 'translateY(30px)',
+                    transition: 'cubic-bezier(.41,.55,.03,.96) 1s',
+                    transitionDelay: `${index * 0.02}s`,
+                  }}
+                >
+                  <BillCard index={index} bill={bill} list={deletedBillsList} />
+                </Box>
+              </ResetStyleWithAnimation>
             ))}
           </MuiList>
 
